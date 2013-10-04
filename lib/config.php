9213496b7b9df671fe64ee74f5e7284b1454cbb6
qinyinyujian <gvvvvy@gmail.com>
2013-10-04 17:14:17 +0800

    把数据库连接的一些数据修改成常量.

--- lib/config.php
+++ lib/config.php
@@ -1,8 +1,9 @@
 <?php
 $wwwtitle = "我的衣柜"; /* 网站标题 */
-$sqln = "127.0.0.1";
-$sqlu = "yigui";
-$sqlp = "wobu1008";
-$sqld = "yigui";
+define("sqln", "127.0.0.1");
+define("sqlu", "yigui");
+define("sqlp", "wobu1008");
+define("sqld", "yigui");
 $dlzt = "0"; //登陆状态判断
+
 ?>
