b42afe71a76785dee8da06abf90e97ed51bc7870
qinyinyujian <gvvvvy@gmail.com>
2013-10-12 14:43:46 +0800

    最基本的上传文件和处理

---
+++ user/upload.php
@@ -0,0 +1,47 @@
+<?php require '../header.php';?>
+<script>
+    $(document).ready(function() {
+        $("#file").change(function() {
+            var image = /.+(.JPEG|.jpeg|.JPG|.jpg|.GIF|.gif|.BMP|.bmp|.PNG|.png)$/;
+            if ($("#file").val() != '' && image.test($("#file").val()) == true) {
+                $("#upimage").submit();
+            } // else {
+               // $("form").find("a").hide();
+            //}
+        });
+        $("#img_kj").load(function() {
+            var data = $(window.frames['img_kj'].document.body).find("img");
+            if ( data != null) {
+                $(".box_img").prepend(data);
+            }
+        });
+    });
+</script>
+<div class="content">
+  <div class="box_img"></div>
+  <div class="updata">
+    <ul>
+      <li>
+        <form id="upimage"action="upload_image.php" method="post" target="img_kj" enctype="multipart/form-data">
+          上传图片:
+          <input type="file" name="image" id="file" style="width:155px"/>
+        </form>
+      </li>
+      <form action="upload_file.php" method="post">
+        <li> 衣服品牌:
+          <input type="text" name="brinds">
+        </li>
+        <li> 上市时间:
+          <input type="month" name="user_date">
+        </li>
+        <li> 衣服货号:
+          <input type="text" name="id">
+        </li>
+        <input type="submit" value="submit">
+      </form>
+    </ul>
+  </div>
+</div>
+</div>
+<iframe style="display:none" id="img_kj" name="img_kj"></iframe>
+<?php require '../footer.php';?>
---
+++ user/upload_image.php
@@ -0,0 +1,9 @@
+<?php
+     //不存在当前上传文件则上传
+    $img_name = md5(uniqid("yigui"));
+    //获取文件后缀
+    $file_houzhui = substr($_FILES['image']['name'],strripos($_FILES['image']['name'],"."));
+    move_uploaded_file($_FILES['image']['tmp_name'],"../image/" . $img_name . $file_houzhui); 
+    //输出图片文件<img>标签
+         echo "<img src=\"/image/$img_name$file_houzhui\" width=\"230\" height=\"250\">";
+?>
