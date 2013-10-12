de5055477fb8dd79213167e1dc2e4b57f11cba12
qinyinyujian <gvvvvy@gmail.com>
2013-10-12 14:42:09 +0800

    修改一些代码

--- header.php
+++ header.php
@@ -7,31 +7,38 @@ require 'authc.php';
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title><?php echo $wwwtitle;?></title>
+<title><?php echo wwwtitle;?></title>
 <!-- SEO -->
 <meta name="description" content="我的衣柜是一个分享和记录自己想要买或者已经拥有的服饰和鞋子。">
 <meta name="keywords" content="衣服,鞋子,首饰,帽子">
-<LINK rel="stylesheet" type="text/css" href="top.css">
+<LINK rel="stylesheet" type="text/css" href="<?php echo www;?>top.css">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>	
 </head>
 
 <body>
-<div class="neirong">
 	<!-- 顶端 -->
 	<div class="top">
 		<!-- logo备用 -->
-		<!--<div class="logo">
-		<img alt="">
-	</div>-->
+		<div class="logo">
+			我的衣柜
+		<!--<img alt="">-->
+	</div>
 		<!-- 检测登陆状态 -->
         <div class="user">
+        	<ul>
 <?php 
 	if (isset($_COOKIE['name'])) {
-		echo "<p>$_COOKIE[name]</p>";
+		echo "<li>$_COOKIE[name]</li>";
 	}
 	else { 
-		echo "<a class=\"login\" href=\"login.php\">登陆</a>  <a class=\"login\" href=\"reg.php\">注册</a>";
+		echo "<li>";
+		echo "<a class=\"login\" href=\"login.php\">登陆</a>";
+		echo "</li>";
+		echo "<li>";
+		echo "<a class=\"login\" href=\"reg.php\">注册</a>";
+		echo "</li>";
 	}	
 ?>
+</ul>
 </div>
 </div>
