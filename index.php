6dd458fdd26304e991d2f7739b3246231a7f8d4b
qinyinyujian <gvvvvy@gmail.com>
2013-10-12 14:42:19 +0800

    修改一些代码

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php require 'header.php'; ?>
 
 <!-- 内容 -->
-<div class="box">
-	<div class="content">
-		<img src="image/oem.jpg" width="250" height="250">
+<div class="content">
+	<div class="box">
+		<img src="image/oem.jpg" width="230" height="220">
+		<h1 class="h1size">MYGB T恤</h1>
+		<p>其他</p>
+	</div>
+    <div class="box">
+		<img src="image/oem.jpg" width="230" height="220">
 		<h1 class="h1size">衣服名称标题</h1>
 		<p>其他</p>
 	</div>
-	
-	
+    <div class="box">
+		<img src="image/oem.jpg" width="230" height="220">
+		<h1 class="h1size">衣服名称标题</h1>
+		<p>其他</p>
+	</div>
+    <div class="box">
+		<img src="image/oem.jpg" width="230" height="220">
+		<h1 class="h1size">衣服名称标题</h1>
+		<p>其他</p>
+	</div>
+    <div class="box">
+		<img src="image/oem.jpg" width="230" height="220">
+		<h1 class="h1size">衣服名称标题</h1>
+		<p>其他</p>
+	</div>
 </div>
+
 <?php require 'footer.php';?>
