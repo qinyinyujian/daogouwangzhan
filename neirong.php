2e5191d83d716c2e59c7ffaff4b9ab04ded45472
qinyinyujian <gvvvvy@gmail.com>
2013-10-12 14:43:21 +0800

    修改一些代码

---
+++ neirong.php
@@ -0,0 +1,10 @@
+<?php
+/* 内容页 */
+
+echo "<div class=\"box\">";
+echo "<img src=\"image/oem.jpg\" width=\"230\" height=\"250\">";
+echo "<h1 class=\"h1size\">衣服名称标题</h1>";
+echo "<p>其他</p>";
+echo "</div>";
+
+?>
